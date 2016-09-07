b30a8326c07b423cdb0d4c9003508c0fcc99cd87
aldavies <aldavies@umich.edu>
2016-09-06 20:17:56 -0400

    testing scope.getweather

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 angular.module('weather', [])
 
-.controller('forecast', ['$http', '$scope', function ($http, $scope){
+.controller('forecast', function ($http, $scope){
 
   const _this = this;
   var default_city = 'New York, NY';
@@ -28,53 +28,10 @@ angular.module('weather', [])
           window.alert('unknown error');
         }
         });
-    };
-    var success = function(response) {
-                    _this.conditions = response.data;
-    };
-
-    const error = function(response) {
-
-    };
-
-    
+    }; 
   });
 
   /***
    *  this method is expected to set the forecast objects
    **/
-  $scope.getWeather = function(city) {
-    var success = function(response) {
-      var data = response.data.query.results.channel;
-
-      $scope.location = data.location;
-      $scope.item = data.item;
-    };
-
-    var error = function(response) {
-    };
-
-    $http.get("http://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='${city}'')&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
-                .then(success, Error);
-  }
-
-  /***
-   *  this method is expected to return the icon for the corresponding condition code
-   *  from the codeToCondition map file
-   **/
-  $scope.getIcon = function(code) {
-    return _this.conditions.filter(condition == condition.code == code)[0].icon;
-  };
-
-  /***
-   * load the condition code map
-   * this maps the condition code returned from the api to the corresponding icon
-   */
-  setTimeout(GetConditionMap,         
-          5000);
-
-  /***
-   * load weather for the default city
-   */
-  // $scope.getWeather(name);
-}]);
+  
