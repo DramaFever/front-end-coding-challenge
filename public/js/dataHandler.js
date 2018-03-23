428528c151c0c30623b17d7152d39def33b71131
Geddy <g3ddylee@gmail.com>
2018-03-23 14:39:32 -0400

    Small sorter that also searches keys.

--- public/js/dataHandler.js
+++ public/js/dataHandler.js
@@ -10,17 +10,29 @@ const extractTags = items => {
   items.forEach(item => {
     tags = tags.concat(item.tags)
   })
-  return tags.unique().sort((a, b) => { return a.toLowerCase() > b.toLowerCase() })
+  return tags.unique().sort(sortCaseInsensitive.bind(this, null))
 }
 
 // Filter by matching tags and sort by the title field.
 const findByTag = (items, tag) => {
   return items.filter(item => item.tags.indexOf(tag) !== -1)
-              .sort((a,b) => a.title > b.title)
+              .sort(sortCaseInsensitive.bind(this, 'title'))
 }
 
 const findById = (items, id) => {
   return items.filter(item => item.id === id)[0] || {}
+}
+
+// Sorting/filter functions.
+// Sorts strings or objects by key.
+const sortCaseInsensitive = (key, item1, item2) => {
+  let a = key ? item1[key].toLowerCase() : item1.toLowerCase()
+  let b = key ? item2[key].toLowerCase() : item2.toLowerCase()
+  if (a === b) {
+    return 0
+  }
+
+  return a.toLowerCase() > b.toLowerCase() ? 1 : -1
 }
 
 export default {
