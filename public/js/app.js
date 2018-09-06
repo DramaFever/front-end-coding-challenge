5fd07be5fabc93188439b6ca40bf93b964873cce
John Politowski <johnp@liveanswer.com>
2018-09-06 11:05:34 -0400

    WBDL Front-End Coding Challenge

--- public/js/app.js
+++ public/js/app.js
@@ -20,29 +20,164 @@ export default class TagBrowserWidget {
   }
 
   setData(data) {
-    this.data = data;
-    console.log('Data fetched', this.data);
+    this.dataSet = {};
+    this.dataSet.data = data;
+
+    console.log('Data fetched', this.dataSet.data);
+
+    // get array of all tags
+    let tags = data.map(function(product) {
+      return product.tags;
+    });
+
+    // flatten the inner arrays
+    let flatTags = [].concat.apply([], tags);
+
+    // remove duplicates and sort
+    this.dataSet.tags = [...(new Set(flatTags))].sort();
+
+    return this.dataSet;
   }
 
-  getElements() {
-    this.tagList = this.config.element.querySelectorAll('.tag-list')[0];
+  getElements(dataSet) {
+    this.dataSet = dataSet;
+    this.dataSet.tagListEl = this.config.element.querySelector('.tag-list');
 
-    //find and store other elements you need
+    return this.dataSet;
   }
 
-  bindEventListeners() {
-    this.tagList.addEventListener('click', this.tagListClicked.bind(this));
+  bindEventListeners(dataSet) {
+    this.dataSet = dataSet;
+    let tags = this.dataSet.tags;
+    let clear = this.config.element.querySelector('.clear-button');
+    clear.addEventListener('click', this.clearAll.bind(this));
 
-    //bind the additional event listener for clicking on a series title
+    this.dataSet.tagListMarkup = tags.map((item) => `
+      <li>
+        <span data-tag="${item}" class="tag is-link">
+          ${item}
+        </span>
+      </li>
+    `).join(" ");
+    this.dataSet.tagListEl.addEventListener('click', this.tagListClicked.bind(this));
+
+
+
+    return this.dataSet;
   }
 
-  render() {
-    //render the list of tags from this.data into this.tagList
+  render(dataSet) {
+    this.dataSet = dataSet;
+    this.dataSet.tagListEl.innerHTML = this.dataSet.tagListMarkup;
+
   }
 
   tagListClicked(event) {
-    console.log('tag list (or child) clicked', event);
-    //check to see if it was a tag that was clicked and render
-    //the list of series that have the matching tags
+
+    // grabbing tags from data attribute
+    let dataTag = event.path[0].getAttribute('data-tag');
+
+    // clear out selected film on click
+    this.dataSet.selectedFilms = [];
+    this.dataSet.filmTitleEl = this.config.element.querySelector('.matching-items-list');
+    this.dataSet.filmTitleEl.innerHTML = '';
+
+    // setting active tag
+    let tagList = this.config.element.querySelectorAll('.tag.is-link');
+    tagList.forEach(function(tag){
+      if(tag.classList.contains('active')){
+        tag.classList.remove('active');
+      }
+    });
+
+    // show selected films if they matches tag
+    if(dataTag){
+
+      // make sure it's not catching parents of the clicked link
+      if(event.path[0].classList.contains('is-link')){
+        event.path[0].classList.add('active');
+      }
+
+      // filter out relevant films
+      this.dataSet.selectedFilms = this.dataSet.data.filter(function(film) {
+        return film.tags.includes(dataTag);
+      });
+
+      // build markup
+      let selectedFilmsMarkup = this.dataSet.selectedFilms.map((film) => `
+        <li data-id="${film.id}" class="is-link">
+          ${film.title}
+        </li>
+      `).join(" ");
+
+      let tagTitle = this.config.element.querySelector('.tag-title');
+      tagTitle.innerHTML = dataTag;
+
+      // populate films
+      this.dataSet.filmTitleEl.innerHTML = selectedFilmsMarkup;
+
+      // clear selected film on click
+      this.dataSet.selectedFilm = '';
+
+      // bind film clicks
+      this.dataSet.filmTitleEl.addEventListener('click', this.filmListClicked.bind(this));
+    }
   }
+
+  clearAll() {
+      // clear out selected film and tags
+      let tagTitle = this.config.element.querySelector('.tag-title');
+      let filmTitle = this.config.element.querySelector('.selected-item');
+      this.dataSet.selectedFilms = [];
+      this.dataSet.filmTitleEl = this.config.element.querySelector('.matching-items-list');
+      this.dataSet.filmTitleEl.innerHTML = '';
+      filmTitle.querySelector('.thumbnail').src = 'http://via.placeholder.com/350x350';
+      filmTitle.querySelector('.subtitle').innerHTML = 'No Series Selected';
+      filmTitle.querySelector('p').innerHTML = '';
+      filmTitle.querySelector('.rating').innerHTML = '';
+      filmTitle.querySelector('.language').innerHTML = '';
+      filmTitle.querySelector('.country').innerHTML = '';
+      filmTitle.querySelector('.type').innerHTML = '';
+      filmTitle.querySelector('.episodes').innerHTML = '';
+      tagTitle.innerHTML = 'No Tag Selected';
+      let tagList = this.config.element.querySelectorAll('.tag.is-link');
+      tagList.forEach(function(tag){
+        if(tag.classList.contains('active')){
+          tag.classList.remove('active');
+        }
+      });
+  }
+
+  filmListClicked(event) {
+    // set selected film to id
+    let filmId = event.path[0].getAttribute('data-id');
+    let filmList = this.config.element.querySelector('.matching-items-list').querySelectorAll('li');
+    filmList.forEach(function(film){
+      if(film.classList.contains('active')){
+        film.classList.remove('active');
+      }
+    });
+
+    // make sure it's not catching parents of the clicked link
+    if(event.path[0].classList.contains('is-link')){
+      event.path[0].classList.add('active');
+    }
+
+    // assign film data in dataSet
+    this.dataSet.selectedFilm = this.dataSet.data.filter(function(film) {
+      return film.id.toString() === filmId;
+    });
+
+    // populate html for film
+    let filmTitle = this.config.element.querySelector('.selected-item');
+    filmTitle.querySelector('.thumbnail').src = this.dataSet.selectedFilm[0].thumbnail;
+    filmTitle.querySelector('.subtitle').innerHTML = this.dataSet.selectedFilm[0].title;
+    filmTitle.querySelector('p').innerHTML = this.dataSet.selectedFilm[0].description;
+    filmTitle.querySelector('.rating').innerHTML = this.dataSet.selectedFilm[0].rating;
+    filmTitle.querySelector('.language').innerHTML = this.dataSet.selectedFilm[0].nativeLanguageTitle;
+    filmTitle.querySelector('.country').innerHTML = this.dataSet.selectedFilm[0].sourceCountry;
+    filmTitle.querySelector('.type').innerHTML = this.dataSet.selectedFilm[0].type;
+    filmTitle.querySelector('.episodes').innerHTML = this.dataSet.selectedFilm[0].episodes;
+  }
+
 }
